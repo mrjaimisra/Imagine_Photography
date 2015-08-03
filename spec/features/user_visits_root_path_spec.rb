a09087e5e72b1b616479aaa72c7fbdce597b57a8
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-03 10:55:56 -0600

    Spec: user visits landing page
    Created landing page

---
+++ spec/features/user_visits_root_path_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "the landing page", type: :feature do
+  context "a user" do
+    it "can access the root path" do
+      visit root_path
+      expect(current_path).to eq root_path
+    end
+  end
+end
