dd7ef027471ddadd5d7de8390d7bd6b6382df640
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-04 11:20:52 -0600

    Implemented Jason's suggestions

--- spec/features/user_views_items_page_spec.rb
+++ spec/features/user_views_items_page_spec.rb
@@ -2,28 +2,26 @@ require "rails_helper"
 
 RSpec.describe "the items view", type: :feature do
   context "a user visits the items view" do
+    let!(:item)  { Item.create(name: "Hamburger and fries", description: "delicious", price: 12, image_url: test_image_url) }
+
     it "displays all items" do
-      item = Item.create(name: "Hamburger and fries", description: "delicious",
-        price: 12, image_url: test_image_url)
       visit items_path
 
       within(".items-list") do
         within("#item-box") do
-          expect(page).to have_content("Hamburger and fries")
+          expect(page).to have_content("#{item.name}")
         end
       end
     end
 
     it "views an items name, description, price and image" do
-      item = Item.create(name: "Hamburger and fries", description: "delicious",
-        price: 12, image_url: test_image_url)
       visit items_path
 
       within(".items-list") do
         within("#item-box") do
-          expect(page).to have_content("Hamburger and fries")
-          expect(page).to have_content("delicious")
-          expect(page).to have_content("12")
+          expect(page).to have_content("#{item.name}")
+          expect(page).to have_content("#{item.description}")
+          expect(page).to have_content("#{item.price}")
           expect(page).to have_css("img[src*='#{item.image_url}']")
         end
       end
