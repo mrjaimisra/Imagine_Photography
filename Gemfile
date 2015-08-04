47f0e83bd4f56bbeebcdda2ab2c06e11df586c47
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-04 17:24:53 -0600

    add seed file

--- Gemfile
+++ Gemfile
@@ -9,7 +9,6 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
-
 gem 'material_design_lite-rails'
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+name = %w(Pizza Hamburger Hotdog Chilli Hummus Vanilla Chocolate Beans Steak Rice)
+description = "Very tasty."
+image_url = "http://i.livescience.com/images/i/000/048/850/i02/capybara-02.jpg?1324347800"
+category_id = rand(3)
+
+name.each do |name|
+  price = rand(15)
+  Item.create(name: name, description: description, price: price, image_url: image_url, category_id: category_id)
+end
