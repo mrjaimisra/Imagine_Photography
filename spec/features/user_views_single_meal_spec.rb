3f56c0590fcddb0f60c848c18254c49a973c349d
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-03 22:27:22 -0600

    Spec: user can view single item

---
+++ spec/features/user_views_single_meal_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "the item view", type: :feature do
+  context "an existing item" do
+    it "views a single item" do
+      item = Item.create(name: "Hamburger and fries", description: "delicious", price: 12)
+      visit items_path
+
+      within(".items-list") do
+        within("#item-box") do
+          click_link "Hamburger and fries"
+        end
+      end
+
+      expect(current_path).to eq item_path(item)
+
+      within(".item-show") do
+        within(".item-info") do
+          expect(page).to have_content "Hamburger and fries"
+          expect(page).to have_content "delicious"
+          expect(page).to have_content "12"
+        end
+      end
+    end
+  end
+end
