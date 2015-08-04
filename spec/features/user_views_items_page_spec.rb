b2606a32bfe53c5f305cce7cd8a1c8f7d16df270
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-03 18:25:49 -0600

    Spec: user views details about each item
    display more info in items view
    
    clean up comments and add config

--- spec/features/user_views_items_page_spec.rb
+++ spec/features/user_views_items_page_spec.rb
@@ -5,9 +5,23 @@ RSpec.describe "the items view", type: :feature do
     it "displays all items" do
       Item.create(name: "Hamburger and fries", description: "delicious", price: 12)
       visit items_path
+
       within(".items-list") do
-        within(".item-box") do
+        within("#item-box") do
           expect(page).to have_content("Hamburger and fries")
+        end
+      end
+    end
+
+    it "views an items name, description, price and image" do
+      Item.create(name: "Hamburger and fries", description: "delicious", price: 12)
+      visit items_path
+
+      within(".items-list") do
+        within("#item-box") do
+          expect(page).to have_content("Hamburger and fries")
+          expect(page).to have_content("delicious")
+          expect(page).to have_content("12")
         end
       end
     end
