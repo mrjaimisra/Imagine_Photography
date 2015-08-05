f0e3bd1f51f1cb556dd25d60ad785d5c85e8088c
Jason Wright <noobjey@gmail.com>
2015-08-05 11:11:37 -0600

    fixed test formatting

--- spec/features/user_views_items_page_spec.rb
+++ spec/features/user_views_items_page_spec.rb
@@ -2,29 +2,29 @@ require "rails_helper"
 
 RSpec.describe "the items view", type: :feature do
   context "a user visits the items view" do
-    let!(:item)  { Item.create(name: "Hamburger and fries",
+    let!(:item) { Item.create(name:        "Hamburger and fries",
                               description: "delicious",
-                              price: 12,
-                              image_url: test_image_url,
+                              price:       12,
+                              image_url:   test_image_url,
                               category_id: 1) }
 
     it "displays all items" do
       visit menu_path
 
-        within(".item-box") do
-          expect(page).to have_content("#{item.name}")
+      within(".item-box") do
+        expect(page).to have_content("#{item.name}")
       end
     end
 
     it "views an items name, description, price and image" do
       visit menu_path
 
-        within(".item-box") do
-          expect(page).to have_content("#{item.name}")
-          expect(page).to have_content("#{item.description}")
-          expect(page).to have_content("#{item.price}")
-          expect(page).to have_css("img[src*='#{item.image_url}']")
-        end
+      within(".item-box") do
+        expect(page).to have_content("#{item.name}")
+        expect(page).to have_content("#{item.description}")
+        expect(page).to have_content("#{item.price}")
+        expect(page).to have_css("img[src*='#{item.image_url}']")
+      end
     end
   end
 end
