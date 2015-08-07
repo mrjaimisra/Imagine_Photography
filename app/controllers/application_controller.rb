06f0ab39692564cdfa5ae32a788fdc055dd43a74
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-06 18:24:21 -0600

    add routes, app ctrler, cart_items ctrler routes: add cart route app ctrler: add session cart method cart_item ctrler #index: gives all items stored in cart session cart_item ctrler #create: add item to cart, set the session cart equal to cart.data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :cart
+
+  def cart
+    @cart ||= Cart.new(session[:cart])
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,18 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-
   delete "/logout", to: "sessions#destroy"
 
   get "/sign_up", to: "users#new"
   post "/sign_up", to: "users#create"
 
-  # resources :users, only: [:index]
-
-  get "/menu", to: "items#index", as: :menu
   namespace :menu do
     resources :categories, only: [:show]
   end
 
+  get "/menu", to: "items#index", as: :menu
   get "meals/:id", to: "items#show", as: :meal
 
-  get "/cart", to: "cart_items#index"
   post "/cart_items", to: "cart_items#create"
+  get "/cart", to: "cart_items#index"
 end
