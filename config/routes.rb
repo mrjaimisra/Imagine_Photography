18b41cdb33627a8b656c2f7ef455d721bbe7b20c
mrjaimisra <misrajai01@gmail.com>
2015-08-06 21:40:37 -0600

    added get route for 'menu/:id' to 'menu/categories#show' and wrote to_param method to add a slug for the category name into the url

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,13 @@ Rails.application.routes.draw do
   get "/sign_up", to: "users#new"
   post "/sign_up", to: "users#create"
 
+  # namespace :menu do
+    get "menu/:id", to: "menu/categories#show"
+  # end
+
   # resources :users, only: [:index]
 
   get "/menu", to: "items#index", as: :menu
-  namespace :menu do
-    resources :categories, only: [:show]
-  end
 
   get "meals/:id", to: "items#show", as: :meal
 
