b61d27a2884f53e4f3c5eceec0f3d00b4013b645
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-06 18:27:26 -0600

    add cart class initialize either with empty hash if a current cart does not exist #items instantiates the items as cart items #add_item sets the value equal to zero if it does not already exist in the hash adds 1 to the value for that items key

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,18 @@
 class Cart
+  attr_reader :data
   def initialize(data)
-    @data = data
+    @data = data || Hash.new
   end
 
   def items
-    @data
+    @data.map do |item_id, quantity|
+      item = Item.find(item_id)
+      CartItem.new(item, quantity)
+    end
+  end
+
+  def add_item(item)
+    data[item.id.to_s] ||= 0
+    data[item.id.to_s] += 1
   end
 end
