ff1f54511b2414b5fdb527ddb03ce697432f4003
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-08 10:29:40 -0600

    Spec: user adds, removes items from cart

---
+++ spec/features/user_modifies_quantity_in_cart_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "the cart quantity", type: :feature do
+  context "a user with items in the cart" do
+    it "adds items to the cart" do
+      item = create_item
+      sign_in
+      visit menu_path
+
+      within(".item-info") do
+        expect(page).to have_content item.name
+        2.times { click_button "Add to Cart" }
+        expect(current_path).to eq menu_path
+      end
+
+      click_link "Cart"
+
+      within(".table-striped") do
+        expect(page).to have_content item.name
+        expect(page).to have_content "2"
+        click_button "+"
+      end
+
+      expect(page).to have_content "3"
+    end
+  end
+
+  it "removes items from the cart" do
+    item = create_item
+      sign_in
+      visit menu_path
+
+      within(".item-info") do
+        expect(page).to have_content item.name
+        2.times { click_button "Add to Cart" }
+        expect(current_path).to eq menu_path
+      end
+
+      click_link "Cart"
+
+      within(".table-striped") do
+        expect(page).to have_content item.name
+        expect(page).to have_content "2"
+        click_button "-"
+      end
+
+    expect(page).to have_content "1"
+  end
+end
