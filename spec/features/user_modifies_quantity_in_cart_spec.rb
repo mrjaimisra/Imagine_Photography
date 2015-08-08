48ef5648c980615cdf9f3f3eec8bd35128402a02
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-08 10:51:23 -0600

    Spec: user views total cost

--- spec/features/user_modifies_quantity_in_cart_spec.rb
+++ spec/features/user_modifies_quantity_in_cart_spec.rb
@@ -27,23 +27,36 @@ RSpec.describe "the cart quantity", type: :feature do
 
   it "removes items from the cart" do
     item = create_item
-      sign_in
-      visit menu_path
+    sign_in
+    visit menu_path
 
-      within(".item-info") do
-        expect(page).to have_content item.name
-        2.times { click_button "Add to Cart" }
-        expect(current_path).to eq menu_path
-      end
+    within(".item-info") do
+      expect(page).to have_content item.name
+      2.times { click_button "Add to Cart" }
+      expect(current_path).to eq menu_path
+    end
 
-      click_link "Cart"
+    click_link "Cart"
 
-      within(".table-striped") do
-        expect(page).to have_content item.name
-        expect(page).to have_content "2"
-        click_button "-"
-      end
+    within(".table-striped") do
+      expect(page).to have_content item.name
+      expect(page).to have_content "2"
+      click_button "-"
+    end
 
     expect(page).to have_content "1"
+  end
+
+  it "displays total cost" do
+    item = create_item
+    sign_in
+    visit menu_path
+
+    2.times { click_button "Add to Cart" }
+    click_link "Cart"
+
+    within(".total") do
+      expect(page).to have_content "Total: 24$"
+    end
   end
 end
