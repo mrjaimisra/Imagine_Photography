59da437b92e02513bc5148ee88dcf6a81b943f82
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-08 21:31:13 -0600

    Spec: user has default role, admin dashboard auth

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,21 +1,45 @@
 require "rails_helper"
 
 RSpec.describe "an admin" do
-  it "can see all users on the dashboard" do
-    admin = User.create(username: "Admin", password: "admin", role: 1)
-    allow ApplicationController.to receive(:current_user).and_return(admin)
+  context "an admin" do
+    it "can visit the admin dashboard" do
+      register_user
+      admin = User.create(username: "Admin", password: "admin", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user)
+        .and_return(admin)
 
-    register_user
-    sign_in
+      click_link "Sign In"
+      within(".login-form") do
+        fill_in "Username", with: admin.username
+        fill_in "Password", with: "admin"
+        click_button "Sign In"
+      end
 
-    visit login_path
-    within(".login-form") do
-      fill_in "Username", with: admin.password
-      fill_in "Password", with: "admin"
-      click_button "Sign In"
+      expect(current_path).to eq menu_path
+      visit "/admin/users"
+
+      expect(page).to eq admin_dashboard_index_path
+
+      within(".nav-header") do
+        expect(page).to have_content "Admin Dashboard"
+      end
     end
+  end
 
-    expect(current_path).to eq(admin_path)
-    expect(page).to have_content("Jason")
+  context "a registered user" do
+    it "can not access the admin dashboard" do
+      register_user
+      sign_in
+
+      visit "/admin/dashboard"
+      expect(current_path).to eq "/404"
+    end
+  end
+
+  context "an unregistered user" do
+    it "can not access the admin dashboard" do
+      visit "/admin/dashboard"
+      expect(current_path).to eq "/404"
+    end
   end
 end
