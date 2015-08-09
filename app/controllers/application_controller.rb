00bd7408f95e3749b03c2ef6868d5b8bef0c6ec9
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-08 22:37:53 -0600

    add current_admin auth base controller: calls #require_admin before each action dashboard: removed redundant code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def current_admin
+  def current_admin?
     current_user && current_user.admin?
   end
 end
