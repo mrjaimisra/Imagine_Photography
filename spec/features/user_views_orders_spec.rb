ce3af2751ea9f9cf7ce2e1029cd2ee578700f3c5
Jason Wright <noobjey@gmail.com>
2015-08-10 14:12:48 -0600

    removed unneeded comments

--- spec/features/user_views_orders_spec.rb
+++ spec/features/user_views_orders_spec.rb
@@ -1,25 +1,3 @@
-# Background: An existing user that has one previous order
-# 
-# As an authenticated user
-# When I visit "/orders"
-# Then I should see my past order
-# And I should see a link to view that order
-
-# And when I click that link
-# Then I should see each item that was ordered with the quantity and line-item subtotals
-# And I should see links to each item's show page
-
-# And I should see the current status of the order (ordered, paid, cancelled, completed)
-# And I should see the total price for the order
-# And I should see the date/time that the order was submitted
-# 
-# If the order was completed or cancelled
-# Then I should see a timestamp when the action took place
-# 
-# And if any of the items in the order were retired from the menu
-# Then they should still be able to access the item page
-# But they should not be able to add the item to their cart
-
 require "rails_helper"
 
 RSpec.describe "a user with one previous order", type: :feature do
