a413ac82a26b9c4bd700f21b7667b522d3de2472
mrjaimisra <misrajai01@gmail.com>
2015-08-10 14:12:23 -0600

    implemented paperclip validations in the item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "400X400>", thumb: "100X100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
   belongs_to :category
   validates :name,
             :description,
