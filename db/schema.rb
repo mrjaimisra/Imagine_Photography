864dbc74dae2209143413b4388bf7898cf972b5a
mrjaimisra <misrajai01@gmail.com>
2015-08-10 22:35:28 -0600

    added image_url column to users table... again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811025904) do
+ActiveRecord::Schema.define(version: 20150811043317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150811025904) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "image_url"
   end
 
   create_table "order_items", force: :cascade do |t|
