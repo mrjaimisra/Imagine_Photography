dbd5fc8955aed1ff228ffffb55f95111f348cd6c
mrjaimisra <misrajai01@gmail.com>
2015-08-13 09:41:31 -0600

    added a capitalization for the names column for both categories and statuses

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Status, type: :model do
+  it "capitalizes the name before saving it to the database" do
+    status = Status.create(name: "HELLO")
+    expect(status.name).to eq("Hello")
+  end
 end
