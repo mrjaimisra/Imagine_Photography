f4936e9eff9c6c5c1a11dd5399cee82a1b9718da
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-15 13:45:29 -0600

    remove unused env variables

--- config/initializers/skylight.rb
+++ config/initializers/skylight.rb
@@ -1,7 +1,1 @@
-Skylight.application: ENV["skylight_application_id"]
 Skylight.authentication: ENV["skylight_authentication_token"]
-
-production:
-  Skylight.authentication = ENV["skylight_production_auth_token"]
-
-
