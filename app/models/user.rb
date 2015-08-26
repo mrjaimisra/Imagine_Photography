2c3bfa47160e03814d7cf439d19df78a198dad87
Michael Reeves <mreeves68@yahoo.com>
2015-08-26 13:40:25 -0600

    Add Roles table and UserRoles join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826191836) do
+ActiveRecord::Schema.define(version: 20150826193008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,11 +51,29 @@ ActiveRecord::Schema.define(version: 20150826191836) do
     t.integer  "status",                               default: 0
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
   create_table "statuses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_roles", ["role_id"], name: "index_user_roles_on_role_id", using: :btree
+  add_index "user_roles", ["user_id"], name: "index_user_roles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
@@ -64,4 +82,6 @@ ActiveRecord::Schema.define(version: 20150826191836) do
   end
 
   add_foreign_key "order_photos", "orders"
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
