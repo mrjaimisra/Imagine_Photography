a131920f25b3ffccdba148869bae2de03fcb9bca
Michael Reeves <mreeves68@yahoo.com>
2015-08-26 14:44:03 -0600

    Add Role query methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,29 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def registered_user?
+    roles.exists?(name: 'registered_user')
+  end
+
+  def store_admin?
+    roles.exists?(name: 'store_admin')
+  end
+
+  def platform_admin?
+    roles.exists?(name: 'platform_admin')
+  end
+
+  def self.registered_users
+    all.joins(:roles).where(roles: { name: "registered_user" })
+  end
+
+  def self.store_admins
+    all.joins(:roles).where(roles: { name: "store_admin" })
+  end
+
+  def self.platform_admins
+    all.joins(:roles).where(roles: { name: "platform_admin" })
+  end
+
 end
