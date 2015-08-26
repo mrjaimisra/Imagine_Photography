2ce365960e727f22aa1b987d135bda6ca4ec2dd7
mrjaimisra <misrajai01@gmail.com>
2015-08-25 20:22:01 -0600

    edit seed file to prepare for new database change enum role for user from site_admin to platform_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, :street_name, :zipcode, :phone_number, presence: true
 
-  enum role: %w(default business_admin photographer site_admin)
+  enum role: %w(default business_admin photographer platform_admin)
 
   scope :customers,      -> { where(role: 0) }
   scope :photographers,  -> { where(role: 2) }
