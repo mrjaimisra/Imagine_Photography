2f1d03c8ec5b96db56605f462ff5b8b3f04add5a
Tyler Graham <tylergraham134@gmail.com>
2015-08-26 18:51:48 -0600

    deletes user role spec and uncommented user visits explore path spec

--- spec/features/user_visits_explore_pages_spec.rb
+++ spec/features/user_visits_explore_pages_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature "User visits explore pages", type: :feature do
- # before do
- #   visit "/explore"
- # end
+RSpec.feature "User visits explore page", type: :feature do
+  before do
+    visit photos_path
+  end
 
- # scenario "successfully" do
- #   within("h1") do
- #     expect(page).to have_content("Explore")
- #   end
- #   expect(page).to have_content("Landscape")
- #   expect(page).to have_xpath("//a[@href=\"/photos/2\"]")
- #   expect(page).to have_selector(".thumbnail", count: 10)
- # end
+  xscenario "successfully" do
+    within("h1") do
+      expect(page).to have_content("Explore")
+    end
+    expect(page).to have_content("Landscape")
+    expect(current_path).to eq('/explore')
+    expect(page).to have_xpath("//a[@href=\"/photos/2\"]")
+    expect(page).to have_selector(".thumbnail", count: 10)
+  end
 end
