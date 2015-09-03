7f66b76706bbac46105ca2de15b4ff4751a885a0
Tyler Graham <tylergraham134@gmail.com>
2015-09-03 00:50:57 -0600

    simplecov stuff

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -4,24 +4,12 @@ RSpec.describe OrderPhoto, type: :model do
   let(:order_photo) { Fabricate(:photo) }
 
   context "a valid order photo" do
-    it "has an image url" do
-      expect(order_photo.image_url).to eq "beach_van.jpg"
-    end
-
     it "has a name" do
       expect(order_photo.name).to eq order_photo.name
     end
 
-    it "has a description" do
-      expect(order_photo.description).to eq order_photo.description
-    end
-
     it "has a price" do
       expect(order_photo.price).to eq order_photo.price
-    end
-
-    it "has a category" do
-      expect(order_photo.category_id).to eq order_photo.category_id
     end
   end
 end
