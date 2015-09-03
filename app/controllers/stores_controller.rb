02b633754955f62ed94f823f6012de01ef060f61
Michael Reeves <mreeves68@yahoo.com>
2015-09-02 19:11:17 -0600

    Add store id and role on store creation

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -10,14 +10,9 @@ class StoresController < ApplicationController
   def create
     @photographer = Store.new(store_params)
     if @photographer.save
+      set_role
+      send_mail
       flash[:success] = "Your application has been received."
-      NotificationsMailer.contact(
-        name: "Imagine Photography",
-        email: "#{current_user.email}",
-        message: "Congratulations! You have successfully created a new business
-        at Imagine Photography! Log in to your account to set up your profile
-        and begin uploading and selling your photos."
-      ).deliver_now
       redirect_to profile_path
     else
       flash.now[:warning] = "There was a problem with your information."
@@ -39,6 +34,20 @@ class StoresController < ApplicationController
   end
 
   private
+    def set_role
+      current_user.roles <<  Role.find_by(name: "store_admin")
+      current_user.update_attribute(:store_id, @photographer.id)
+    end
+
+    def send_mail
+      NotificationsMailer.contact(
+        name: "Imagine Photography",
+        email: "#{current_user.email}",
+        message: "Congratulations! You have successfully created a new business
+        at Imagine Photography! Log in to your account to set up your profile
+        and begin uploading and selling your photos."
+      ).deliver_now
+    end
 
     def store_params
       params.require(:store).permit(:name, :email, :avatar, :header)
